77b6d2b9df359e14052151eeb87e4f8b8309c771
Francois Zaninotto <fzaninotto@gmail.com>
2010-10-16 17:57:05 +0200

    fixed level1 to make it work

--- level1.php
+++ level1.php
@@ -1,29 +1,29 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=test';
+$dsn = 'mysql:host=localhost;dbname=bookstore';
 $user = 'fzaninotto';
 $pass = 'S3Cr3t';
 
 try {
   
   // opening the connection
-  $dbh = new PDO($dsn, $user, $pass);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $dbh->beginTransaction();
+  $con = new PDO($dsn, $user, $pass);
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $con->beginTransaction();
 
   // inserting an author
   $sql = 'INSERT INTO `author` (`first_name`, `last_name`)
     VALUES (:first_name, :last_name)';
-  $sth = $dbh->prepare($sql);
+  $sth = $con->prepare($sql);
   $sth->bindValue(':first_name', 'Leo', PDO::PARAM_STR);
   $sth->bindValue(':last_name', 'Tolstoi', PDO::PARAM_STR);
   $sth->execute();
 
   // inserting a couple books
-  $author_id = $con->lastInsertId;
+  $author_id = $con->lastInsertId()	;
   $sql = 'INSERT INTO `book` (`title`, `author_id`)
     VALUES (:title, :author_id)';
-  $sth = $dbh->prepare($sql);
+  $sth = $con->prepare($sql);
   $sth->bindValue(':title', 'War and Peace', PDO::PARAM_STR);
   $sth->bindValue(':author_id', $author_id, PDO::PARAM_INT);
   $sth->execute();
@@ -38,7 +38,7 @@ try {
     WHERE `author`.`last_name` = ?
     AND `book`.`title` LIKE ? 
     LIMIT 1';
-  $sth = $dbh->prepare($sql);
+  $sth = $con->prepare($sql);
   $sth->bindValue(1, 'Tolstoi', PDO::PARAM_STR);
   $sth->bindValue(2, 'War%', PDO::PARAM_STR);
   $sth->execute();
@@ -51,7 +51,7 @@ try {
   // selecting all books
   $sql = 'SELECT `book`.`id`, `book`.`title`, `book`.`author_id`
     FROM `book`';
-  $sth = $dbh->prepare($sql);
+  $sth = $con->prepare($sql);
   $sth->execute();
   $nbResults = 0;
   while ($row = $sth->fetch(PDO::FETCH_NUM)) {
@@ -65,13 +65,13 @@ try {
   }
   
   // commit the transaction
-  $dbh->commit();
+  $con->commit();
   
   // close the connection
-  $dbh = null;
+  $con = null;
   
 } catch (PDOException $e) {
   
-  $dbh->rollBack();
+  $con->rollBack();
   printf("Failed: %s\n", $e->getMessage());
 }

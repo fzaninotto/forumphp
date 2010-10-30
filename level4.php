202d0ccd5f8f4fa2988654802ec172cab96c4f27
Francois Zaninotto <fzaninotto@gmail.com>
2010-10-31 00:47:03 +0200

    added table inheritance in level4

--- level4.php
+++ level4.php
@@ -30,10 +30,10 @@ echo $books->isEmpty(); // false
 echo $books->count(); // 2
 echo $books->getFirst()->getTitle(); // Harry Potter and the Philosopher's Stone
 foreach ($books as $book) { 
-	printf('<li class="%s">%s</li>',
-		$books->isOdd() ? 'odd' : 'even',
-		$book->getTitle()
-	);
+  printf('<li class="%s">%s</li>',
+    $books->isOdd() ? 'odd' : 'even',
+    $book->getTitle()
+  );
 }
 //<li class="even">Harry Potter and the Philosopher's Stone</li>
 //<li class="odd">Harry Potter and the Chamber of Secrets</li>
@@ -44,11 +44,11 @@ $books []= $book1;
 // Query termination methods
 echo BookQuery::create()->count(); // 2
 BookQuery::create()
-	->filterByTitle('Harry Potter and the%')
-	->update(array('Title' => 'Voldemort got you Pwned'));
+  ->filterByTitle('Harry Potter and the%')
+  ->update(array('Title' => 'Voldemort got you Pwned'));
 AuthorQuery::create()
-	->filterByLastName('Rowling')
-	->delete();
+  ->filterByLastName('Rowling')
+  ->delete();
 echo BookQuery::create()->count(); // 0
 
 // Identity Map
@@ -66,15 +66,15 @@ $author = AuthorQuery::create()->findPk($pk); // no query
 
 // On-Demand Hydration
 for ($i=0; $i < 1000; $i++) { 
-	$book = new Book();
-	$book->setTitle('book #' . $i);
-	$book->save();
+  $book = new Book();
+  $book->setTitle('book #' . $i);
+  $book->save();
 }
 $books = BookQuery::create()
-	->setFormatter(ModelCriteria::FORMAT_ON_DEMAND)
-	->find(); // no hydration yet
+  ->setFormatter(ModelCriteria::FORMAT_ON_DEMAND)
+  ->find(); // no hydration yet
 foreach ($books as $book) {
-	echo $book->getTitle(), "\n"; // on demand hydration
+  echo $book->getTitle(), "\n"; // on demand hydration
 }
 BookQuery::create()->forceDeleteAll();
 
@@ -86,21 +86,19 @@ $book1->save();
 $book2 = new Book();
 $book2->setTitle('This title is unique');
 if ($book2->validate()) { // false
-	$book2->save();
+  $book2->save();
 } else {
-	foreach($book2->getValidationFailures() as $failure) {
-		echo $failure->getMessage() , "\n"; // Book title already in database.
-	}
+  foreach($book2->getValidationFailures() as $failure) {
+    echo $failure->getMessage() , "\n"; // Book title already in database.
+  }
 }
 
 // Behaviors
-
 // soft delete a book
 $book1->delete();
 echo $book1->isDeleted(); // false
 echo $book1->getDeletedAt(); // 2010-10-29 18:14:23
 echo BookQuery::create()->count(); // 0
-
 // recover a deleted book
 BookQuery::disableSoftDelete();
 echo BookQuery::create()->count(); // 1
@@ -109,39 +107,29 @@ $book = BookQuery::create()->findOne();
 $book->unDelete();
 BookQuery::enableSoftDelete();
 echo BookQuery::create()->count(); // 1
-
 // soft delete works on queries
 BookQuery::create()->deleteAll();
 
 // Table Inheritance
+$art = new Article();
+$art->setTitle('Avatar Makes Best Opening Weekend in the History');
+$art->setIsPublished(true);
+$art->setBody("With $232.2 million worldwide total,
+Avatar had one of the best-opening weekends in the history of cinema.");
+$art->save();
+// create a new Video
+$vid = new Video();
+$vid->setTitle('Avatar Trailer');
+$art->setIsPublished(true);
+$vid->setUrl('http://www.avatarmovie.com/index.html');
+$vid->save();
+
+$contents = ContentQuery::create()->find();
+foreach ($contents as $content) {
+  echo $content->getTitle(), "\n";
+}
+// Avatar Makes Best Opening Weekend in the History
+// Avatar Trailer
 
 // Autocompletion
 
-/* And also:
-
-	 * Full Query logging
-	 * Hooks
-	 * Nested Sets
-	 * Many-to-Many and One-to-one relationships
-	 * Reverse engineering
-	 * Runtime introspection
-	 * Packages
-	 * Nested transactions
-	 * Master-slave
-	 * Pager utility
-	 * Query cache
-	 * Multi-column foreign keys
-	 * Cascade emulation for ON DELETE and ON UPDATE
-	 * Indexes
-	 * Lazy-loaded columns
-	 * LOB Support
-	 * View
-	 * Sequences
-	 
-  And even more to come in 1.6:
-	 * Migrations
-	 * XML, JSON, YAML and CSV parser and dumper
-	 * Joins with multiple conditions
-	 * Schemas
-
- */
